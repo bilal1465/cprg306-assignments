f3b78e9888ffb9799072bae654627b995a6ad41b
bilal <sarwar.bilal1465@gmail.com>
2023-09-25 18:35:35 -0600

    week 2 assignment completed

---
+++ app/StudentInfo.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+export default function StudentInfo() {
+    return (
+        <div>
+            <p>Name: Bilal Dhillon</p>
+            <p>Course Section: CPRG 306 C</p>
+            <Link href="https://github.com/bilal1465/cprg306-assignments">https://github.com/bilal1465/cprg306-assignments</Link>
+        </div>
+    )
+}
---
+++ app/week2/page.js
@@ -0,0 +1,10 @@
+import StudentInfo from "../StudentInfo"
+
+export default function week2() {
+    return (
+        <div>
+        <h1>My Shopping List</h1>
+        <StudentInfo/>
+        </div>
+    )
+}
